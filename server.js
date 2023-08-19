49b4714cd885f37f45fb7d285a2e1814a05779f8
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-08-19 12:48:00 +0530

    CHG | Server Setup COmplete

--- server.js
+++ server.js
@@ -9,11 +9,19 @@ require('dotenv').config();
 const app = express()
 
 // db
+// mongoose
+//     .connect(process.env.MONGO_URL, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: true,
+//     })
+//     .then(() => console.log("DB CONNECTED"))
+//     .catch((err) => console.log("DB CONNECTION ERR", err));
+
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: true,
+        useUnifiedTopology: true,
     })
     .then(() => console.log("DB CONNECTED"))
     .catch((err) => console.log("DB CONNECTION ERR", err));
@@ -24,5 +32,14 @@ app.use(bodyParser.json({ limit: "2mb"}));
 app.use(cors());
 
 // route
+app.get("/api", (req, res) => {
+    res.json({
+        data: "hey you hit node API",
+    });
+});
 
+//port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
