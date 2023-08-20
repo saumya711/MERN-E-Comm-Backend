568a5b2c3388c5d2c48bee2ad15280b2650572e9
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-08-20 16:00:22 +0530

    CHG | Controllers and User Schema

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,8 @@
 const express = require("express");
+const { createOrUpdateUser } = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get("/create-or-update-user", (req, res) => {
-    res.json({
-        data: "hey you hit create-or-update-user API rndpoint",
-    });
-});
+router.get("/create-or-update-user", createOrUpdateUser);
 
 module.exports = router;
