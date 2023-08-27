eb7512ee74d62b8d31e2a767ffb671e415ce68f5
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-08-27 20:51:14 +0530

    CHG | Create or Update User -  Controller Function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,25 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.json({
-        data: "Hey you hit Create o Update User API rndpoint",
-    });
+const User = require("../models/userModel");
+
+exports.createOrUpdateUser = async (req, res) => {
+    const { email, name, picture } = req.user;
+
+    const user = await User.findOneAndUpdate(
+        { email }, 
+        { name, picture}, 
+        { new: true}
+    );
+
+    if(user) {
+        console.log("USER UPDATED", user);
+        res.json(user);
+    } else {
+        const newUser = await new User({
+            email,
+            name,
+            picture
+        }).save();
+        console.log("USER CREATED", newUser);
+        res.json(newUser);
+    }
+
 }
