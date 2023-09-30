2e7a7072e0df75c84b93022234803b9a310c2475
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-09-30 22:00:10 +0530

    CHG | Category Route and Controller

---
+++ routes/category.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+// middleware
+const { authCheck, adminCheck } = require("../middlewares/authMiddleware");
+
+// controller
+const { create, read, update, remove, list} = require("../controllers/categoryController");
+
+// routes
+router.post("/category", authCheck, adminCheck, create);
+router.get("/categories", list);
+router.get("/category/:slug", authCheck, adminCheck, read);
+router.put("/category/:slug", authCheck, adminCheck, update);
+router.delete("/category/:slug", authCheck, adminCheck, remove);
+
+
+module.exports = router;
