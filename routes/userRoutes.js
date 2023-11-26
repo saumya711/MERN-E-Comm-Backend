233fc76171e05dfcd9e741eaed530729d6f0c155
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-11-26 22:18:17 +0530

    CHG | Save Cart to Database

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/user", (req, res) => {
-    res.json({
-        data: "hey you hit user API rndpoint",
-    });
-});
+// middleware
+const { authCheck } = require("../middlewares/authMiddleware");
+
+// controller
+const { userCart } = require("../controllers/userController");
+
+
+router.post("/cart", authCheck, userCart);
+
+// router.get("/user", (req, res) => {
+//     res.json({
+//         data: "hey you hit user API rndpoint",
+//     });
+// });
 
 module.exports = router;
